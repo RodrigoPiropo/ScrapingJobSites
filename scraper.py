db096fe9c3ec8f269d001c5ed15615e7533f5442
RodrigoPiropo <rodrigoap1307@gmail.com>
2020-12-18 08:35:35 -0300

    Separated use of classes

--- scraper.py
+++ scraper.py
@@ -88,18 +88,3 @@ class Search:
             contents.append(content)
 
         return contents
-
-sites = [
-["https://br.indeed.com", "https://br.indeed.com/empregos?q={}&l={}",
- "div.jobsearch-SerpJobCard", "a", False, "h2.title a", "span.company",
-  "span.salaryText"]
-]
-
-websites = []
-for site in sites:
-    websites.append(WebSite(site[0], site[1], site[2], site[3],
-                            site[4], site[5], site[6], site[7]))
-
-contents = Search("Python", "São Paulo").parse(websites[0])
-for content in contents:
-    print(content.dict())
---
+++ using.py
@@ -0,0 +1,16 @@
+from scraper import WebSite, Search
+
+sites = [
+["https://br.indeed.com", "https://br.indeed.com/empregos?q={}&l={}",
+ "div.jobsearch-SerpJobCard", "a", False, "h2.title a", "span.company",
+  "span.salaryText"]
+]
+
+websites = []
+for site in sites:
+    websites.append(WebSite(site[0], site[1], site[2], site[3],
+                            site[4], site[5], site[6], site[7]))
+
+contents = Search("Python", "São Paulo").parse(websites[0])
+for content in contents:
+    print(content.dict())
